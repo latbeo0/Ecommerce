a2ee51c88c6fa554eb5bac1d03777a44916f0135
Khang Vo <vohoankhangw12@gmail.com>
2022-06-30 15:19:08 +0700

    update ProductManagement

--- client/src/pages/cms/components/ProductManagement/ProductManagement.jsx
+++ client/src/pages/cms/components/ProductManagement/ProductManagement.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ProductList from "../ProductList/ProductList";
-import "./ProductManagement.css"
+import BasicSelect from "../../../../components/Basic/BasicSelect";
+import "./ProductManagement.css";
 const ProductManagement = () => {
   return (
     <div className="ProductContainer">
-      <h1>Product</h1>
-      <ProductList/>
+      <div className="ProductContent">
+        <h1>Product</h1>
+        <div className="SelectContainer">
+          <BasicSelect className = "ProductSelect"/>
+          <BasicSelect className = "ProductSelect"/>
+          <BasicSelect className = "ProductSelect"/>
+        </div>
+        <ProductList />
+      </div>
     </div>
   );
 };
